49632fc5066b4f7be8805c1ad7e6cf43bba66fca
Augustine200 <austineemekacares@gmail.com>
2022-12-08 13:43:07 +0000

    printind size of datatypes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Return: Success.
+ */
+int main(void)
+{
+printf("Size of char: %d byte(s)\n", sizeof(char));
+printf("Size of int: %d byte(s)\n", sizeof(int));
+printf("Size of long int: %d bytes(s)\n", sizeof(long int));
+printf("Size of long long int: %d bytes(s)\n", sizeof(long long int));
+printf("Size of float: %d byte(s)\n", sizeof(float));
+return (0);
+}
