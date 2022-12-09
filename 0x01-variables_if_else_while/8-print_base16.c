108319f55bb7bdd75e683874f530c904a1d63699
Augustine200 <austineemekacares@gmail.com>
2022-12-09 15:35:57 +0000

    numbers and alphabet

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry.
+ *
+ * Return: Success.
+ */
+int main(void)
+{
+int num;
+char c;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+for (c = 'a'; c <= 'f'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
