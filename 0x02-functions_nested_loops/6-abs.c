3258ad213a976c11d3fabba90d82d917eebce489
Augustine200 <austineemekacares@gmail.com>
2022-12-13 14:38:13 +0000

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Prints the absolute value of a number
+ * @n: number to be checked.
+ * Return: SUccess.
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+n = n + n * 2;
+_putchar(n);
+}
+else
+{
+_putchar(n);
+}
+return (n);
+}
