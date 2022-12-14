4e53795390029fbcc7934db64955730d21c58aef
Augustine200 <austineemekacares@gmail.com>
2022-12-14 11:50:54 +0000

    printing natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,22 +11,31 @@ int i, j;
 for (i = n; i <= 98; i++)
 {
 putchar(i);
-putchar(',');
-putchar(' ');
+putchar(32);
+putchar(44);
 if (i == 98)
+{
 break;
+}
 else
 {
-putchar((i / 10) + '0');
-putchar((i % 10) + '0');
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
 }
 }
 for (j = n; j >= 98; j--)
 {
 putchar(j);
-putchar(',');
-putchar(' ');
+putchar(32);
+putchar(44);
 if (j == 98)
+{
 break;
+}
+else
+{
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
+}
 }
 }
