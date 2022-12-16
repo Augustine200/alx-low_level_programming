1574b88c04d28d74fcb198165cef61301ef96e75
Augustine200 <austineemekacares@gmail.com>
2022-12-16 04:58:54 +0000

    to print diagonal

--- 0x04-more_functions_nested_loops/func.c
+++ 0x04-more_functions_nested_loops/func.c
@@ -1,46 +1,17 @@
 #include <stdio.h>
-
 int main(void)
 {
-	int i, j, k;
+int i, j;
+for (i = 0; i <= 10; i++)
+{
+for (j = 0; j >= i; j--)
+{
+if (i == j || i + j == 10 - 1)
+{
+putchar('\\');
+}
+else
 
-		for (j = '0', i = '1'; j <= '9', i <= '4'; j++, i ++{
-				int i, j, k;
-				for (i = '1'; i < '2'; i++)
-				{
-				for (j = '0'; j <= '9'; j++)
-				{
-				for (k = '0'; k <= '9'; k++)
-				{
-				if (i == '1' && j == '4')
-				{
-				break;
-				}
-				else
-				{
-				_putchar(i);
-				_putchar(j);
-				}
-				}
-				}
-				_putchar('\n');
-				}
-		)
-		{
-			for (k = '0'; k <= '9'; k++)
-			{
-				if (i == '1' && j == '4')
-				{
-					break;
-				}
-				else
-				{
-					putchar(k);
-				        putchar(j);
-				}
-			}
-		}
-
-		}putchar('\n');
-	}
-
+putchar('\n');
+}
+}
