1f4bf82f2ed4b7e15c5ff828037405aab8c914ca
Augustine200 <austineemekacares@gmail.com>
2022-12-19 17:34:08 +0000

    checks for length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - the length of a string.
+ * @s: The string that its length will be checked.
+ * Return: Success.
+ */
+int _strlen(char *s)
+{
+strlen(s);
+}
