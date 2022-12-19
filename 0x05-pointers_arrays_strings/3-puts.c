ba3345bf2cc29d28e004f094a2f7d4ce53bec570
Augustine200 <austineemekacares@gmail.com>
2022-12-19 18:44:52 +0000

    printing strings with puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - It prints a string.
+ * @str: The string.
+ * Return: Success.
+ */
+void _puts(char *str)
+{
+while (*str != '\0')
+puts(str);
+}
