6be25a996600203b6a9c4783faec2f1a62c53f37
Augustine200 <austineemekacares@gmail.com>
2022-12-20 14:42:18 +0000

    prints other characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts2 - prints even character.
+ * @str: The string.
+ * Return: Success.
+ */
+void puts2(char *str)
+{
+int i = 0;
+while (*str != '\0')
+{
+if (i % 2 == 0)
+{
+_putchar(*str);
+}
+else
+{
+continue;
+}
+i++;
+*str++;
+}
+}
